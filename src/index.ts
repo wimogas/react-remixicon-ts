e4a5e8b805a1d8801242668e4f412dc878335b19
Guillem <wimogas@gmail.com>
2023-07-05 20:47:34 +0200

    fully functioning first version of the package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 import Icon from './Icon';
 
-export * from './icons/Arrows';
-export * from './icons/Health';
-export * from './icons/Media';
-export * from './icons/System';
-export * from './icons/User';
 export { Icon };
